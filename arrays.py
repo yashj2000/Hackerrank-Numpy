226aeff4663e02a6294772a081a7482da73384e5
Yash Janyani <54151714+yashj2000@users.noreply.github.com>
2020-12-31 18:09:07 +0530

    Create arrays.py

---
+++ arrays.py
@@ -0,0 +1,10 @@
+import numpy
+
+def arrays(arr):
+    # complete this function
+    # use numpy.array
+    a=numpy.array(arr[::-1],dtype='float')
+    return a
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
